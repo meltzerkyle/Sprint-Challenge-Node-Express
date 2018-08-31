007dd03d263c5ba6037b9fa419b32be88303e1c0
Kyle Meltzer <jimmy13@Kyles-MacBook-Pro.local>
2018-08-31 11:07:12 -0500

    completed GETS for actions and projects, starting on POST

--- index.js
+++ index.js
@@ -9,23 +9,123 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("This is working");
+  res.send("This is working.");
 });
 
+//Projects CRUD Ops
+
 app.get("/projects", (req, res) => {
+  projects
+    .get()
+    .then(projects => {
+      res.status(200).json(projects);
+    })
+    .catch(err => {
+      console.log("error", err);
+      res
+        .status(500)
+        .json({ error: "The projects information could not be retrieved." });
+    });
+});
+
+app.get("/projects/:id", (req, res) => {
+  const { id } = req.params;
+  projects
+    .get(id)
+    .then(project => {
+      console.log(project);
+      if (project.id) {
+        res.status(200).json(project);
+      } else {
+        res.status(404).json({
+          message: "The project with the specified ID does not exist."
+        });
+      }
+    })
+    .catch(err => {
+      console.log("error", err);
+      res
+        .status(500)
+        .json({ error: "The project information could not be retrieved." });
+    });
+});
+
+app.get("/projects/:id/actions", (req, res) => {
+  const { id } = req.params;
+  projects
+    .getProjectActions(id)
+    .then(actions => {
+      if (actions.length > 0) {
+        res.status(200).json(actions);
+      } else {
+        res.status(404).json({ message: "The project has no actions." });
+      }
+    })
+    .catch(err => {
+      console.log("error", err);
+      res
+        .status(500)
+        .json({ error: "The actions information could not be retrieved." });
+    });
+});
+
+app.post("/projects", (req, res) => {
+  const newProject = req.body;
+
+  if (newProject.name && newProject.description) {
     projects
-      .get()
-      .then(projects => {
-        res.status(200).json(projects);
+      .insert(newProject)
+      .then(project => {
+        res.status(201).json(project);
       })
       .catch(err => {
         console.log("error", err);
-        res
-          .status(500)
-          .json({ error: "The projects information could not be retrieved." });
+        res.status(500).json({
+          error: "There was an error while saving the project to the database."
+        });
       });
-  });
+  } else {
+    res.status(400).json({
+      error: "Please provide a name and a description for the project."
+    });
+  }
+});
 
+//Actions CRUD Ops
 
+app.get("/actions", (req, res) => {
+  actions
+    .get()
+    .then(actions => {
+      res.status(200).json(actions);
+    })
+    .catch(err => {
+      console.log("error", err);
+      res
+        .status(500)
+        .json({ error: "The actions information could not be retrieved." });
+    });
+});
+
+app.get("/actions/:id", (req, res) => {
+  const { id } = req.params;
+  actions
+    .get(id)
+    .then(action => {
+      if (action) {
+        res.status(200).json(action);
+      } else {
+        res.status(404).json({
+          message: "The action with the specified ID does not exist."
+        });
+      }
+    })
+    .catch(err => {
+      console.log("error", err);
+      res
+        .status(500)
+        .json({ error: "The action information could not be retrieved." });
+    });
+});
 
 app.listen(8000, () => console.log("\n== API on port 8k==\n"));
